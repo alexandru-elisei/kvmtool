0ac3584517db591343e3dea261dbaa79cd6d4f41
Alexandru Elisei <alexandru.elisei@arm.com>
2020-01-07 14:47:05 +0000

    vfio/pci: Don't write configuration value twice
    After writing to the device fd as part of the PCI configuration space
    emulation, we read back from the device to make sure that the write
    finished. The value is read back into the PCI configuration space and
    afterwards, the same value is copied by the PCI emulation code. Let's
    read from the device fd into a temporary variable, to prevent this
    double write.
    
    The double write is harmless in itself. But when we implement
    reassignable BARs, we need to keep track of the old BAR value, and the
    VFIO code is overwritting it.

--- vfio/pci.c
+++ vfio/pci.c
@@ -470,7 +470,7 @@ static void vfio_pci_cfg_write(struct kvm *kvm, struct pci_device_header *pci_hd
 	struct vfio_region_info *info;
 	struct vfio_pci_device *pdev;
 	struct vfio_device *vdev;
-	void *base = pci_hdr;
+	u32 tmp;
 
 	if (offset == PCI_ROM_ADDRESS)
 		return;
@@ -490,7 +490,7 @@ static void vfio_pci_cfg_write(struct kvm *kvm, struct pci_device_header *pci_hd
 	if (pdev->irq_modes & VFIO_PCI_IRQ_MODE_MSI)
 		vfio_pci_msi_cap_write(kvm, vdev, offset, data, sz);
 
-	if (pread(vdev->fd, base + offset, sz, info->offset + offset) != sz)
+	if (pread(vdev->fd, &tmp, sz, info->offset + offset) != sz)
 		vfio_dev_warn(vdev, "Failed to read %d bytes from Configuration Space at 0x%x",
 			      sz, offset);
 }
